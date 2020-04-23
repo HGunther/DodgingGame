dd20eeee57f74be79198302967146cfcaacb37c9
hgunther <hnsgunther@gmail.com>
2020-04-23 11:46:34 -0600

    Added mobile touch controls

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -21,10 +21,46 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float velocity = inputX * speed;
-        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+        float keyboardInput = Input.GetAxisRaw("Horizontal");
+        float touchInput = getTouchInput();
 
+        float velocity;
+        if (Mathf.Abs(touchInput) > Mathf.Abs(keyboardInput))
+        {
+            var maxMovement = speed * Time.deltaTime;
+            if (maxMovement > Mathf.Abs(touchInput))
+            {
+                velocity = touchInput;
+            }
+            else
+            {
+                velocity = speed * Time.deltaTime * (touchInput / Mathf.Abs(touchInput));
+            }
+        }
+        else
+        {
+            velocity = keyboardInput * speed * Time.deltaTime;
+        }
+
+        transform.Translate(Vector2.right * velocity);
+
+        KeepInsideBounds();
+    }
+
+    float getTouchInput()
+    {
+        //var rawMouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        //return rawMouseX - transform.position.x;
+        if (Input.touches.Length > 0)
+        {
+            var difference = Input.touches[0].position.x - transform.position.x;
+            return difference;
+        }
+        return 0;
+    }
+
+    void KeepInsideBounds()
+    {
         if (transform.position.x - halfPlayerWidth < -screenHalfWidthInWorldUnits)
         {
             var newX = -screenHalfWidthInWorldUnits + halfPlayerWidth;
